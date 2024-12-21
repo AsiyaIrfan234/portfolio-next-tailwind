776ce8f8c067520bc4ccffde5eb7c8cc2d726681
AsiyaIrfan234 <160402332+AsiyaIrfan234@users.noreply.github.com>
2024-12-21 22:51:48 +0500

    Refactor Projects component: Updated image usage with Next.js Image component for optimization, added AOS animations for enhanced user experience.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div id="about"className='container pt-32'>
-        <h2 className='text-3xl md:text-5xl font-bold' data-aos="zoom-in-up">About Me</h2>
-        <p className='text-gray-500 pt-4' data-aos="zoom-in-up">
-Hello! I'm Asiya Irfan, an aspiring developer with a deep passion for coding and building impactful digital experiences. Currently, I'm enhancing my skills in TypeScript, Next.js, and Python, aiming to create dynamic, scalable web applications that push the boundaries of full-stack development. I am dedicated to learning and exploring new technologies to stay at the forefront of the ever-evolving tech landscape. My goal is to contribute to innovative projects and continuously grow as a versatile and proficient developer.
-        </p>
+    <div id="about" className="container pt-32">
+      <h2 className="text-3xl md:text-5xl font-bold" data-aos="zoom-in-up">About Me</h2>
+      <p className="text-gray-500 pt-4" data-aos="zoom-in-up">
+        Hello! I&apos;m Asiya Irfan, an aspiring developer with a deep passion for coding and building impactful digital experiences. Currently, I&apos;m enhancing my skills in TypeScript, Next.js, and Python, aiming to create dynamic, scalable web applications that push the boundaries of full-stack development. I am dedicated to learning and exploring new technologies to stay at the forefront of the ever-evolving tech landscape. My goal is to contribute to innovative projects and continuously grow as a versatile and proficient developer.
+      </p>
     </div>
-  )
-}
-export default About
+  );
+};
+
+export default About;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
 
 const Hero = () => {
   return (
-    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/banner-bg.png)] bg-cover'
-    style={{backgroundSize: "35%", backgroundPosition: "left 100px top 100px"}}
+    <div
+      id="hero"
+      className="min-h-screen bg-no-repeat bg-[url(/banner-bg.png)] bg-cover"
+      style={{ backgroundSize: '35%', backgroundPosition: 'left 100px top 100px' }}
     >
-        <Navbar />
-        <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-        <div className='hidden lg:block'></div>
+      <Navbar />
+      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
+        <div className="hidden lg:block"></div>
 
-        <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center text-white'>
+        <div className="text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center text-white">
           <div>
-            <p data-aos="zoom-in-up">I'm</p>
+            <p data-aos="zoom-in-up">I&apos;m</p>
             <p data-aos="zoom-in-up">Asiya</p>
             <p data-aos="zoom-in-up">Irfan</p>
-            </div>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
